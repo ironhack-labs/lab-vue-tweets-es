48676db169b2b8d9dc7eef65a7b559dbc7be2136
Elena Vida <elenavidaluc3a@gmail.com>
2022-08-14 18:27:52 +0200

    iteration 3 to 8

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,28 +1,14 @@
 <template>
   <div className="tweet">
-    <img :src="tweet.user.image" className="profile" alt="profile" />
+    <profile-image :image="tweet.user.image" />
 
     <div className="body">
       <div className="top">
-        <span className="user">
-          <span className="name">{{ tweet.user.name }}</span>
-          <span className="handle">@{{ tweet.user.handle }}</span>
-        </span>
-
-        <span className="timestamp">{{ tweet.timestamp }}</span>
+        <user :user="tweet.user" />
+        <timestamp :timestamp="tweet.timestamp" />
       </div>
-
-      <p className="message">
-        {{ tweet.message }}
-      </p>
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <message :message="tweet.message" />
+      <actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -30,8 +16,15 @@
 </template>
 
 <script>
+import ProfileImage from "./ProfileImage.vue";
+import User from "./User.vue";
+import Timestamp from "./Timestamp.vue";
+import Message from "./Message.vue";
+import Actions from "./Actions.vue";
+
 export default {
   props: ["tweet"],
+  components: { ProfileImage, User, Timestamp, Message, Actions },
 };
 </script>
 
